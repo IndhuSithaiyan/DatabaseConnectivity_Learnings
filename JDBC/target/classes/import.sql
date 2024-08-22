6646be85d7cdd6ea4b760e026664314bfad5b906
IndhuSithaiyan <104045806+IndhuSithaiyan@users.noreply.github.com>
2024-08-22 19:42:23 +0530

    Delete JDBC/target/classes directory

--- JDBC/target/classes/import.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists EMPLOYEES (id identity primary key, name varchar (100) not null);
-
-truncate table EMPLOYEES;
-
-insert into EMPLOYEES (id, name) values (1, 'John');
-insert into EMPLOYEES (id, name) values (2, 'Lisa');
